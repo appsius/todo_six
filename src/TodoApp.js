8bbbb36c02e8a1a4a28f5621cc5d42238a571ae3
Appsius <ilyas.ustunkaya234434@gmail.com>
2020-01-15 16:14:13 +0300

    Adding Todo Item Component

--- src/TodoApp.js
+++ src/TodoApp.js
@@ -32,8 +32,12 @@ export default function TodoApp() {
           <Typography color='inherit'>TODOS WITH MY HOOKS</Typography>
         </Toolbar>
       </AppBar>
-      <TodoForm addTodo={addTodo} />
-      <TodoList todos={todos} />
+      <Grid container justify='center' sytle={{ marginTop: '1rem' }}>
+        <Grid item xs={11} md={8} lg={4}>
+          <TodoForm addTodo={addTodo} />
+          <TodoList todos={todos} />
+        </Grid>
+      </Grid>
     </Paper>
   );
 }
