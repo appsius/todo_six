d20efd732b4c7e16d403deba578c9f9159bef91f
Appsius <ilyas.ustunkaya234434@gmail.com>
2020-01-16 20:11:56 +0300

    Custom Hook: Reducer + LocalStorage

---
+++ src/hooks/useLocalStorageReducer.js
@@ -0,0 +1,21 @@
+import { useReducer, useEffect } from 'react';
+
+function useLocalStorageReducer(key, defaultVal, reducer) {
+  // make piece of state, based off of the value in localstorage
+  const [state, dispatch] = useReducer(reducer, defaultVal, () => {
+    let val;
+    try {
+      val = JSON.parse(window.localStorage.getItem(key) || String(defaultVal));
+    } catch (e) {
+      val = defaultVal;
+    }
+    return val;
+  });
+  useEffect(() => {
+    window.localStorage.setItem(key, JSON.stringify(state));
+  }, [key, state]);
+
+  return [state, dispatch];
+}
+
+export default useLocalStorageReducer;
